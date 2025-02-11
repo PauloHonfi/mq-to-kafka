fce7c9aa0ad752870f124d7874036c717fa5dbec
Paulo Honfi <contato@paulohonfi.com.br>
2025-02-11 18:29:20 -0300

    Projeto subindo com hello-world

---
+++ src/main/java/br/com/phs/santander/mq/app/resource/TestResource.java
@@ -0,0 +1,17 @@
+package br.com.phs.santander.mq.app.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestResource {
+
+    @GetMapping
+    public ResponseEntity<String> transferSchecule() {
+        return ResponseEntity.ok().body("Sucesso");
+    }
+
+}
